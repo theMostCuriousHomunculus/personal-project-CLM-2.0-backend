39c39227fa817324ca285e80dfc07361e6fed2c0
Casey Johnson <johnson.casey1211@gmail.com>
2021-02-17 11:12:20 -0600

    hiding sendgrid api key

--- utils/sendgrid-transporter.js
+++ utils/sendgrid-transporter.js
@@ -3,7 +3,7 @@ import sendgridTransport from 'nodemailer-sendgrid-transport';
 
 const transporter = nodemailer.createTransport(sendgridTransport({
   auth: {
-    api_key: `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`
+    api_key: process.env.SENDGRID_API_KEY
   }
 }));
 
